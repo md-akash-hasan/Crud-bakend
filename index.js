2ea4089a837abbffe39612e1e1cfadb019f73490
mdakash102210-wq <mdaksh102210@gmail.com>
2026-05-19 18:13:28 +0600

    dane

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("destinationrec");
     const destioncollactionrec = db.collection("recap");
 
@@ -62,7 +62,7 @@ async function run() {
       res.json(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
